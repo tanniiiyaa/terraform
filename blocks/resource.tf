ce9f0c38d7ec098fa8e748107fe2074ffe8d4195
Tan <ntaniyaa7@gmail.com>
2025-11-25 20:17:25 +0530

    ex2

--- blocks/resource.tf
+++ blocks/resource.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "kuromi" {
 
     instance_type = "t3.micro"
     ami = "ami-0d2aad4794ad02001"
-    key_name = "default"
-    vpc_security_group_ids = [ "sg-0164b8995e136fdd8" ]
+    key_name = "terra"
+    vpc_security_group_ids = [ "sg-024eddf4176040594" ]
 }
