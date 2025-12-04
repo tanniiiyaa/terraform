98d61ecb8dc2622f31d4bb35eba5bce9530a9165
Tan <ntaniyaa7@gmail.com>
2025-12-04 18:22:25 +0530

    new

--- block1/main.tf
+++ block1/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "mage_interface" {
   instance_type           = var.mage_interface_instance_type
   ami                     = var.mage_interface_ami
   key_name                = var.mage_interface_key_name
-   vpc_security_group_ids = var.mage_interface_vpc_security_group_ids
+  vpc_security_group_ids = var.mage_interface_vpc_security_group_ids
   disable_api_termination = var.mage_interface_disable_api_termination
   count                   = var.mage_interface_count
 }
