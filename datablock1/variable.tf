9e1000b5cf2b5c6b61634b083da8b85897c1b457
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-05 22:02:42 +0530

    io

---
+++ datablock1/variable.tf
@@ -0,0 +1,20 @@
+variable "ps2_ami" {
+  default = "ami-0d2aad4794ad02001"
+}
+
+variable "ps2_instance_type" {
+  default = "t3.micro"
+}
+
+variable "ps2_key_name" {
+  default = "new"
+}
+
+variable "ps2_vpc_security_group_ids" {
+  type    = list(string)
+  default = ["sg-05d8d9380629ad735"]
+}
+
+variable "ps2_disable_api_termination" {
+  default = false
+}
