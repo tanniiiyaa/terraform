fd0fc9a9d4a50bf7dacd5e7bc3159caca0ad9e43
tanniiiyaa <ntaniyaa&gmail.com>
2025-12-05 15:12:32 +0000

    #nn

--- block4_datablock/variable.tf
+++ block4_datablock/variable.tf
@@ -1,15 +1,15 @@
 variable "ps2_ami" {
-   default = "ami-0d2aad4794ad02001"
+  default = "ami-0d2aad4794ad02001"
 }
 variable "ps2_instance_type" {
-   default = "t3.micro"
+  default = "t3.micro"
 }
 variable "ps2_key_name" {
-   default = "new"
+  default = "new"
 }
 variable "ps2_vpc_security_group_ids" {
-   default = "sg-05d8d9380629ad735"
+  default = "sg-05d8d9380629ad735"
 }
 variable "ps2_disable_api_termination" {
-   default = false
+  default = false
 }
