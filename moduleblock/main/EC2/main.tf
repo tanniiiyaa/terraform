cebdff062a94936f249ed8f2791c52dfc5febf1f
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-09 20:37:37 +0530

    upp

--- moduleblock/main/EC2/main.tf
+++ moduleblock/main/EC2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webserver" {
     instance_type= var.webserver_instance_type
     key_name = var.webserver_key_name
     subnet_id = var.webserver_subnetA
-    vpc_security_groups_ids = [ var.webserver_sg ]
+    vpc_security_group_ids = [ var.webserver_sg ]
     disable_api_termination = var.webserver_disable_api_termination
 
 
