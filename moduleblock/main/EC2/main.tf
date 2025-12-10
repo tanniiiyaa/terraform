ed08cccee75f9fb72c4073dbc1dffeac42a33ddf
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-10 08:53:50 +0530

    nano

--- moduleblock/main/EC2/main.tf
+++ moduleblock/main/EC2/main.tf
@@ -1,18 +1,19 @@
+/*
 resource "aws_instance" "webserver" {
     ami = var.webserver_ami
-    instance_type= var.webserver_instance_type
+    instance_type = var.webserver_instance_type
     key_name = var.webserver_key_name
     subnet_id = var.webserver_subnetA
     vpc_security_group_ids = [ var.webserver_sg ]
     disable_api_termination = var.webserver_disable_api_termination
 
-
     user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install nginx
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
-              echo "<h1> V strom </h1>" > /var/www/html/index.html
-              EOF
+        #!/bin/bash
+        sudo apt update -y
+        sudo apt install nginx
+        sudo systemctl start nginx
+        sudo systemctl enable nginx
+        echo "<h1> V strom </h1>" > /var/www/html/index.html
+    EOF
 }
+*/
