66984aba2f2a472d7f14e23bf03925c643bd9d4b
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-10 08:55:43 +0530

    nano

--- moduleblock/main/EC2/output.tf
+++ moduleblock/main/EC2/output.tf
@@ -1,9 +1,9 @@
+/*
 output "webserver_pubip" {
-
-    value = aws_instance.webserver.public_ip
+  value = aws_instance.webserver.public_ip
 }
 
 output "webserver_public_dns" {
-
-    value  = aws_instance.webserver.public_dns
+  value = aws_instance.webserver.public_dns
 }
+*/
