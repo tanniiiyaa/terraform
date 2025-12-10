9ae604067d9b0f2befaf69f8af2dfaf0d90df15a
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-10 21:47:02 +0530

    tfvars

---
+++ tfvars/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+     region = "eu-north-1"
+     profile = "configs"
+}
+
+
+resource "aws_instance" "demo" {
+         instance_type = var.instance_type
+         ami = var.ami
+         key_name = var.key_name
+         vpc_security_group_ids = [ var.sg ]
+}
---
+++ tfvars/var.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ami" {
+    default = "ami-0fa91bc90632c73c9"
+}
+
+variable "key_name"{
+    default = "nana"
+
+}
+
+variable "sg" {
+    default = "sg-05d8d9380629ad735"
+}
