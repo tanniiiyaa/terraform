05c090d3e784a215f27b5e68de41581344a090ce
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-11 19:29:11 +0530

    berry

---
+++ backend/ec2.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend = "s3" {
+        bucket = "tf-demo-buckyy"
+        key = "terraform.tfstate"
+        dynamodb_table = "dynao-demo"
+        region = "eu-north-1"
+        profile = "configs"
+        shared_credentials_files = ["/home/tan/.aws/credentials"]
+    }
+}
+
+resource "aws_instance" "sunflower" {
+    instance_type = "t3.micro"
+    ami = "ami-0fa91bc90632c73c9"
+    key_name = "nana"
+    vpc_security_group_ids = [ "sg-05d8d9380629ad735" ]
+}
