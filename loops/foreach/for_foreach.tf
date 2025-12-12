7d16e6f27049bc180a88f2082458106a1f761859
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-12 21:37:26 +0530

    uwu

---
+++ loops/foreach/for_foreach.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+     region = "eu-north-1"
+     profile = "configs"
+}
+
+resource "aws_instance" "plum" {
+    instance_type = "t3.micro"
+    ami = each.value
+    for_each = toset(var.ami_foreach)
+}
+
+variable "ami_foreach" {
+
+    default = default = [ "ami-0b46816ffa1234887" , "ami-0fa91bc90632c73c9" ]
+
+}
+
+resource "aws_iam_user" "iam" {
+    name = each.value
+    for_each = toset(var.iamrole)
+}
+
+variable = "iamrole" {
+    default = [ "ming" , "mini" , "meoww" ]
+}
+
+output "aws_id" {
+
+    value = [
+        for amiid in var.ami_foreach:
+        aws_instance.plum[amiid].id
+    ]
+}
+
+
+COUNT 
+
+1) IAM USER
+
+provider "aws" {
+    region = "eu-north-1"
+    profile = "configs"
+}
+##creating users
+
+variable "userlist" {
+    default = [ "buttercup" , "blossom" , "bubbles" ]
+}
+
+resource "aws_iam_user" "iamuser" {
+    name = var.userlist[count.index]
+    count = length(var.userlist)
+
+}
