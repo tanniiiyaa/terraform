187b22276075214fba1266f7b0a047562032b9b1
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-12 15:45:59 +0000

    my local changes

--- loops/count/count-instance.tf
+++ loops/count/count-instance.tf
@@ -1,24 +1,24 @@
 provider "aws" {
-    region = "eu-north-1"
-    profile = "configs"
+  region  = "eu-north-1"
+  profile = "configs"
 }
 
 #This block creates two identical EC2 instances using the same AMI.
 
 resource "aws_instance" "onion" {
-    instance_type = "t3.micro"
-    ami = "ami-0fa91bc90632c73c9"
-    count = 2
+  instance_type = "t3.micro"
+  ami           = "ami-0fa91bc90632c73c9"
+  count         = 2
 }
 
 ## Creates multiple EC2 instances, each with a different AMI, using count and list indexing.
 
 resource "aws_instance" "tomato" {
-    instance_type = "t3_micro"
-    ami = var.ami_list[count.index]
-    count = length(var.ami_list) 
+  instance_type = "t3.micro"
+  ami           = var.ami_list[count.index]
+  count         = length(var.ami_list)
 }
 
 variable "ami_list" {
-    default = [ "ami-0b46816ffa1234887" , "ami-0fa91bc90632c73c9" ]
+  default = ["ami-0b46816ffa1234887", "ami-0fa91bc90632c73c9"]
 }
