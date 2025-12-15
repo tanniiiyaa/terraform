4104bbe6d2e901d70dc818e221f924fbf1294f42
Tan <ntaniyaa7@gmail.com>
2025-12-15 22:23:01 +0530

    berry

--- loops/count/iam.tf
+++ loops/count/iam.tf
@@ -6,13 +6,6 @@ variable "userlist" {
 }
 
 resource "aws_iam_user" "iamuser" {
-<<<<<<< HEAD:loops/count/count-iam-users.tf
   name  = var.userlist[count.index]
   count = length(var.userlist)
 }
-=======
-    name = var.userlist[count.index]
-    count = length(var.userlist)
-
-}
->>>>>>> b6eba45d72efc0e836bc7b489d2f8af5d14d3fd6:loops/count/iam.tf
