ba34592e7d086023ac6a052e5eaf829e5f95e1c6
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-15 15:38:48 +0000

    sega

--- Provisioners/provisioners.tf
+++ Provisioners/provisioners.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
 resource "aws_instance" "ginger" {
     instance_type = "t3.micro"
-    ami = ami-0b46816ffa1234887
+    ami = "ami-0b46816ffa1234887"
     key_name = "new"
-    vpc_security_group_ids = "sg-05d8d9380629ad735"
+    vpc_security_group_ids = [ "sg-05d8d9380629ad735" ]
 
     provisioner "file" {
         source = "textdata.txt"
